3b41d4a50c85ee5a4b2b807231c19519d7183b57
Scott Pramuk <pramukscott1@gmail.com>
2023-02-16 14:47:29 -0800

    Added placeholder barlet properties

--- components/barlet.js
+++ components/barlet.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, { useState } from "react";
+import reactDom from "react-dom";
+import { Text, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-web";
 
-const Barlet = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-        }}>
-        <Text>B</Text>
-        </View>
-    );
+const barLetStyle = {
+  // Placeholder style, change as necessary
+  backgroundColor: "white",
+  color: "White",
+  flex: "1",
+  alignItems: "center",
+  width: "85%",
 };
 
-export default Barlet;
+function Barlet({ icon }) {
+  return (
+    <View
+      key={icon.id}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TouchableOpacity style={barLetStyle}>
+        <Text style={{ fontFamily: "Roboto", fontSize: "50px" }}>
+          {icon.name}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
+export default Barlet;
