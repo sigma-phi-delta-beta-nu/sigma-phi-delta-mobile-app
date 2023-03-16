f891578dbae862e84f8b9e43f9be11033e799f03
lamkin1 <jamesclamkin@gmail.com>
2023-03-16 15:52:44 -0700

    updating screens

---
+++ screens/Directory.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function Directory () {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>
+            Directory!!! 
+        </Text>
+      </View>
+    )
+
+
+}
+export default Directory;
---
+++ screens/Forms.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function Forms () {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>
+          Forms!!!
+        </Text>
+      </View>
+    )
+
+
+}
+export default Forms
