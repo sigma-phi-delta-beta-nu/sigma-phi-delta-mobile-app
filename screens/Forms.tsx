321f0d1282895119b5fdf040c5ba4cf17b92d743
SammySmash613 <36828580+SammySmash613@users.noreply.github.com>
2023-03-26 16:55:55 -0700

    Update Forms.tsx

--- screens/Forms.tsx
+++ screens/Forms.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
-import { Text, View, Linking } from 'react-native';
+import { Text, View, Linking, StyleSheet } from 'react-native';
 
 const form1 = 'https://forms.gle/g6hna7PL6YPG6SqFA'
+const description1 = "Social Winter 2023 Feedback"
+const deadline1 = "4/1/23"
+
 const form2 = 'https://forms.gle/36kXxP1ka42bvE1z5'
+const description2 = "Beach Cleanup Sign Up"
+const deadline2 = "3/17"
+
 const form3 = 'https://forms.gle/vLZsxWwZjeQa7Akw6'
+const description3 = "Spring Formal RSVP"
+const deadline3 = "ASAP"
 
 function Forms () {
     return (
@@ -11,33 +19,41 @@ function Forms () {
                      justifyContent: 'center', 
                      alignItems: 'center' }}>
         
-        <Text style={{color: 'red', textDecorationLine: 'underline'}}
+        <Text style={styles.linklets}
               onPress={() => Linking.openURL(form1)}>
-          Social Winter 2023 Feedback
+          {description1}
         </Text>
         <Text>
-          Due Date: 4/1/23{"\n\n"}
+          Due Date: {deadline1}{"\n\n"}
         </Text>
 
-        <Text style={{color: 'red', textDecorationLine: 'underline'}}
+        <Text style={styles.linklets}
               onPress={() => Linking.openURL(form2)}>
-          Beach Cleanup Sign Up
+          {description2}
         </Text>
         <Text>
-          Due Date: 3/17{"\n\n"}
+          Due Date: {deadline2}{"\n\n"}
         </Text>
 
-        <Text style={{color: 'red', textDecorationLine: 'underline'}}
+        <Text style={styles.linklets}
               onPress={() => Linking.openURL(form3)}>
-          Spring Formal RSVP
+          {description3}
         </Text>
         <Text>
-          Due Date: ASAP{"\n\n"}
+          Due Date: {deadline3}{"\n\n"}
         </Text>
 
       </View>
     )
 
-
 }
+
+const styles = StyleSheet.create({
+    linklets: {
+      color: "red",
+      textDecorationLine: "underline"
+    }
+
+})
+
 export default Forms
