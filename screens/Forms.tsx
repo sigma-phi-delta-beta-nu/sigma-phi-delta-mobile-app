541f42a78013c4588fab9de6ca2a357621495878
Sammy P <36828580+spaykel@users.noreply.github.com>
2023-05-17 16:35:53 -0700

    Update Forms.tsx

--- screens/Forms.tsx
+++ screens/Forms.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, Linking, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, Linking, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 
 const form1 = 'https://forms.gle/g6hna7PL6YPG6SqFA'
 const description1 = "Social Winter 2023 Feedback"
@@ -17,35 +17,41 @@ const form4 = 'https://forms.gle/Fy3kUGKrVCgat71r6'
 const description4 = "SPD Dye Tournament Signup"
 const deadline4 = "5/5/23"
 
-function Forms () {
-    return (
-      <View style={styles.container}>
-        
-        <Text style={styles.title}>Forms</Text>
+const form5 = 'https://forms.gle/FyQRiRsz6N3XpkYZ6'
+const description5 = "Composite Expected Attendance Form"
+const deadline5 = "5/6/23"
 
-        <MyButton url={form3} buttonText={description3} />
-        <Text style={styles.deadlineCo}>
-          Due Date: {deadline3}{"\n\n"}
-        </Text>
+function Forms() {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Forms</Text>
 
-        <MyButton url={form2} buttonText={description2} />
-        <Text style={styles.deadlineCo}>
-          Due Date: {deadline2}{"\n\n"}
-        </Text>
+      <MyButton url={form3} buttonText={description3} />
+      <Text style={styles.deadlineCo}>
+        Due Date: {deadline3}{"\n\n"}
+      </Text>
 
-        <MyButton url={form1} buttonText={description1} />
-        <Text style={styles.deadlineCo}>
-          Due Date: {deadline1}{"\n\n"}
-        </Text>
+      <MyButton url={form2} buttonText={description2} />
+      <Text style={styles.deadlineCo}>
+        Due Date: {deadline2}{"\n\n"}
+      </Text>
 
-        <MyButton url={form4} buttonText={description4} />
-        <Text style={styles.deadlineCo}>
-          Due Date: {deadline4}{"\n\n"}
-        </Text>
+      <MyButton url={form1} buttonText={description1} />
+      <Text style={styles.deadlineCo}>
+        Due Date: {deadline1}{"\n\n"}
+      </Text>
 
-      </View>
-    )
+      <MyButton url={form4} buttonText={description4} />
+      <Text style={styles.deadlineCo}>
+        Due Date: {deadline4}{"\n\n"}
+      </Text>
 
+      <MyButton url={form5} buttonText={description5} />
+      <Text style={styles.deadlineCo}>
+        Due Date: {deadline5}{"\n\n"}
+      </Text>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -56,30 +62,27 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#fff',
-    // textAlign: 'center',
     fontSize: 16,
     padding: 8
   },
   linklets: {
-      color: "red",
-      textDecorationLine: "underline"
-    },
+    color: "red",
+    textDecorationLine: "underline"
+  },
   deadlineCo: {
-      color: "white"
+    color: "white"
   },
   title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        color: "white"
-    },
-  container: { 
-        flex: 1,
-        // justifyContent: 'left', 
-        // alignItems: 'left', 
-        padding: 20,
-        backgroundColor: '#101010'
-    }
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: "white"
+  },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: '#101010'
+  }
 });
 
 interface MyButtonProps {
@@ -105,4 +108,4 @@ const MyButton: React.FC<MyButtonProps> = ({ url, buttonText }) => {
   );
 };
 
-export default Forms
+export default Forms;
